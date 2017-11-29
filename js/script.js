1f77017e5821a4da46fef521892c2ed44137ddb1
laurakurtzberg <laura.kurtzberg@gmail.com>
2017-11-29 16:00:56 -0500

    loading feature

--- js/script.js
+++ js/script.js
@@ -1,37 +1,7 @@
-;(function(){
-  function id(v){ return document.getElementById(v); }
-  function loadbar() {
-    var ovrl = id("overlay"),
-        prog = id("progress"),
-        stat = id("progstat"),
-        img = document.images,
-        c = 0,
-        tot = img.length;
-    if(tot == 0) return doneLoading();
-
-    function imgLoaded(){
-      c += 1;
-      var perc = ((100/tot*c) << 0) +"%";
-      prog.style.width = perc;
-      stat.innerHTML = "Loading "+ perc;
-      if(c===tot) return doneLoading();
-    }
-    function doneLoading(){
-      ovrl.style.opacity = 0;
-      setTimeout(function(){
-        ovrl.style.display = "none";
-      }, 1200);
-    }
-    for(var i=0; i<tot; i++) {
-      var tImg     = new Image();
-      tImg.onload  = imgLoaded;
-      tImg.onerror = imgLoaded;
-      tImg.src     = img[i].src;
-    }
-  }
-  document.addEventListener('DOMContentLoaded', loadbar, false);
-}());
-
+$(window).ready(function() {
+    setTimeout(function() {$('#loading').hide()}, 1000);
+    // $('#loading').hide();
+});
 
 var fadeInStart=300 // 100px scroll or less will equiv to 1 opacity
     ,fadeInUntil=550 // 200px scroll or more will equiv to 0 opacity
